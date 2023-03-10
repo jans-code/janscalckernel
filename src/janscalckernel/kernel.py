b75e6688f2e273ad226d7a724862bf4cf169e128
jans-code <7jantb@gmx.de>
2023-03-10 16:09:11 +0100

    cleaned unnecessities

--- src/janscalckernel/kernel.py
+++ src/janscalckernel/kernel.py
@@ -31,7 +31,6 @@ class janscalckernel(Kernel):
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {'status': 'ok',
-                # The base class increments the execution count
                 'execution_count': self.execution_count,
                 'payload': [],
                 'user_expressions': {},
